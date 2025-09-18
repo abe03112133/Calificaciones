e95e252a10568f9b7c35f6e8c41b0591126f2344
abe03112133 <al03112133@tecmilenio.mx>
2025-09-17 21:17:58 -0600

    creacion del proyecto

---
+++ src/main/java/com/tecm/calificaciones/Calificaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tecm.calificaciones;
+
+/**
+ *
+ * @author g_mdz
+ */
+public class Calificaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
